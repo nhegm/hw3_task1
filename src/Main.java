95541a3847844fab0f062bd6139352debb078c95
nhegm <nhegmus@gmail.com>
2023-04-17 20:34:56 +0800

    program finished

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 7830;
+        int bonus = price / 20;
+        System.out.println("Вы получите: "+bonus+" бонусных рублей");
+    }
+}
