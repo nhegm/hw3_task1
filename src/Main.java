a3218e23587afdc50f5197648b45c71f8848f7d6
nhegm <nhegmus@gmail.com>
2023-04-19 20:20:26 +0800

    reformated code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int price = 7830;
         int bonus = price / 20;
-        System.out.println("Вы получите: "+bonus+" бонусных рублей");
+        System.out.println("Вы получите: " + bonus + " бонусных рублей");
     }
 }
